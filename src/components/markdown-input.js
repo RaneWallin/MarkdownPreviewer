cb42e68edaa59a87fc75c631dcfe5329a7a49dfa
Rane Wallin <rane.wallin@gmail.com>
2017-04-08 12:42:55 -0700

    removing html input

--- src/components/markdown-input.js
+++ src/components/markdown-input.js
@@ -26,6 +26,7 @@ class MarkdownInput extends Component {
 	};
 
 	handleChange(textInput) {
+		textInput = textInput.replace(/(<([^>]+)>)/ig,"");
 		this.setState({textInput});
 		if(this.props.onChange) {
 			this.props.onChange(textInput);
