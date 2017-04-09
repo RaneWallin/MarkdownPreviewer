fb6cc901e618aa8b7bc72feec8212d8e91012f29
Rane Wallin <rane@ranewallin.com>
2017-04-09 07:51:02 -0700

    testing code

--- src/components/markdown-preview.js
+++ src/components/markdown-preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MarkdownPreview = ({inputText}) => {
 		inputText = inputText.replace(/(<([^>]+)>)/ig,"");
-		//console.log(inputText);
+		console.log(inputText);
 
 		let marked = require('marked');
 
